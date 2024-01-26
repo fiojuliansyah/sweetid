14ba82af5acc651483d32e17ee363c93a8f0ff4a
Fio Juliansyah <fiojuliansyah@gmail.com>
2024-01-26 09:34:50 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -38,10 +38,7 @@ Route::get('/', function () {
   return view('welcome');
 });
 
-Route::get('/generate', function(){
-  \Illuminate\Support\Facades\Artisan::call('storage:link');
-  echo 'ok';
-});
+Route::get('/linkstorage', function () { $targetFolder = base_path().'/storage/app/public'; $linkFolder = $_SERVER['DOCUMENT_ROOT'].'/storage'; symlink($targetFolder, $linkFolder); });
 
 Route::get('home', [HomeController::class, 'index']);
 Route::get('/classes', [HomeController::class, 'products'])->name('products');
