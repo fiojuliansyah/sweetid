55aea13541cdd0e904958bc125ab71a0caadf205
Fio Juliansyah <fiojuliansyah@gmail.com>
2024-03-08 13:49:38 +0700

    Update transactions-table.blade.php

--- resources/views/admin/livewire/transactions-table.blade.php
+++ resources/views/admin/livewire/transactions-table.blade.php
@@ -42,7 +42,6 @@
                                         <th>Amount</th>
                                         <th>Type</th>
                                         <th>Status</th>
-                                        <th></th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -65,20 +64,6 @@
                                         <td><a href="javascript:void(0)" class="btn btn-success btn-rounded light">Paid</a></td>
 
                                         @endif
-                                        <td>
-                                            <div class="dropdown dropstart">
-                                                <a href="javascript:void(0);" class="btn-link" data-bs-toggle="dropdown" aria-expanded="false">
-                                                    <svg width="24" height="24" viewbox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                        <path d="M12 13C12.5523 13 13 12.5523 13 12C13 11.4477 12.5523 11 12 11C11.4477 11 11 11.4477 11 12C11 12.5523 11.4477 13 12 13Z" stroke="#575757" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                        <path d="M12 6C12.5523 6 13 5.55228 13 5C13 4.44772 12.5523 4 12 4C11.4477 4 11 4.44772 11 5C11 5.55228 11.4477 6 12 6Z" stroke="#575757" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                        <path d="M12 20C12.5523 20 13 19.5523 13 19C13 18.4477 12.5523 18 12 18C11.4477 18 11 18.4477 11 19C11 19.5523 11.4477 20 12 20Z" stroke="#575757" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                    </svg>
-                                                </a>
-                                                <div class="dropdown-menu">
-                                                    <a class="dropdown-item" href="transaction-details.html"><i class="las la-info-circle scale5 me-3"></i>View Details</a>
-                                                </div>
-                                            </div>
-                                        </td>
                                     </tr>
                                     @endforeach
                                 </tbody>
