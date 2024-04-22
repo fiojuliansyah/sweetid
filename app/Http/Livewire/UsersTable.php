35df17e198f02357f8bc42e51c234f89ff3d4b5e
Fio Juliansyah <fiojuliansyah@gmail.com>
2024-04-22 17:15:00 +0700

    no

--- app/Http/Livewire/UsersTable.php
+++ app/Http/Livewire/UsersTable.php
@@ -23,6 +23,7 @@ class UsersTable extends Component
         if ($this->search != '') {
             $data = User::where('name', 'like', '%' . $this->search . '%')
                 ->orWhere('email', 'like', '%' . $this->search . '%')
+                ->orWhere('phone', 'like', '%' . $this->search . '%')
                 ->paginate(10);
         } else {
             $data = User::paginate(10);
--- resources/views/admin/livewire/users-table.blade.php
+++ resources/views/admin/livewire/users-table.blade.php
@@ -1,15 +1,16 @@
 <div>
     <div class="table-responsive">
         @if ($message = Session::get('success'))
-        <div class="alert alert-success">
-            <p>{{ $message }}</p>
-        </div>
+            <div class="alert alert-success">
+                <p>{{ $message }}</p>
+            </div>
         @endif
         <div class="row">
             <div class="mb-3 col-md-4">
                 <div class="input-group search-area">
-                    <input wire:model="search" id="search" type="text" class="form-control" name="keyword" placeholder="Search...">
-                </div>   
+                    <input wire:model="search" id="search" type="text" class="form-control" name="keyword"
+                        placeholder="Search...">
+                </div>
             </div>
         </div>
         <table class="table header-border table-responsive-sm">
@@ -18,6 +19,7 @@
                     <th scope="col">No</th>
                     <th scope="col">Name</th>
                     <th scope="col">Email</th>
+                    <th scope="col">Phone</th>
                     <th scope="col">Roles</th>
                     <th scope="col">Action</th>
                 </tr>
@@ -28,24 +30,26 @@
                         <td>{{ ($data->currentPage() - 1) * $data->perpage() + $loop->index + 1 }}</td>
                         <td>{{ $user->name }}</td>
                         <td>{{ $user->email }}</td>
+                        <td>{{ $user->phone }}</td>
                         <td>
-                            @if(!empty($user->getRoleNames()))
-                                @foreach($user->getRoleNames() as $v)
+                            @if (!empty($user->getRoleNames()))
+                                @foreach ($user->getRoleNames() as $v)
                                     <span class="badge badge-success">{{ $v }}</span>
                                 @endforeach
                             @endif
                         </td>
-                        <td>    
-                            <form action="{{ route('users.destroy',$user->id) }}" method="POST">
-                                <a class="btn btn-xs btn-success" href="{{ route('users.class',$user->id) }}" target="_blank">List Class</a>
-                                <a class="btn btn-xs btn-primary" href="{{ route('users.show',$user->id) }}">Show</a>
+                        <td>
+                            <form action="{{ route('users.destroy', $user->id) }}" method="POST">
+                                <a class="btn btn-xs btn-success" href="{{ route('users.class', $user->id) }}"
+                                    target="_blank">List Class</a>
+                                <a class="btn btn-xs btn-primary" href="{{ route('users.show', $user->id) }}">Show</a>
                                 @can('user-edit')
-                                <a class="btn btn-xs btn-warning" href="{{ route('users.edit',$user->id) }}">Edit</a>
+                                    <a class="btn btn-xs btn-warning" href="{{ route('users.edit', $user->id) }}">Edit</a>
                                 @endcan
                                 @csrf
                                 @method('DELETE')
                                 @can('user-delete')
-                                <button type="submit" class="btn btn-xs btn-danger">Delete</button>
+                                    <button type="submit" class="btn btn-xs btn-danger">Delete</button>
                                 @endcan
                             </form>
                         </td>
